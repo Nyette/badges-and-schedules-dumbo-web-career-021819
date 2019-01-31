ee841657adc4d54ac77e105616f04b23a9ee187d
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 08:15:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+def badge_maker(name)
+  badge_message = "Hello, my name is #{name}."
+  return badge_message
+end
+
+def batch_badge_creator(array_names)
+  array_badge_messages = []
+  array_names.each do |name|
+    badge_message = badge_maker(name)
+    array_badge_messages << badge_message
+  end
+  return array_badge_messages
+end
+
+def assign_rooms(array_names)
+  array_room_assignments = []
+  room_number = 1
+  index_of_names = 0
+    while room_number < 8
+      room_assignment = "Hello, #{array_names[index_of_names]}! You'll be assigned to room #{room_number}!"
+      array_room_assignments << room_assignment
+      room_number += 1
+      index_of_names += 1
+    end
+  end
+  return array_room_assignments
+end
+
+
+
+
